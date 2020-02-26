d4fb81c8ec4d1a06f76166d35637c42b1473e42b
ANDERSON <ANDERSON@DESKTOP-0GMJR24>
2020-02-26 14:01:31 -0500

    segundo

---
+++ src/main/java/edu/udec/figurasgeometricas/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.udec.figurasgeometricas;
+
+/**
+ *
+ * @author ANDERSON
+ */
+public class NewClass {
+    
+}
